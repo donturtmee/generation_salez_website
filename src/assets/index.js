8eb5765bed9340fd095945bec051820af5345fc4
robertandreiboboc <andreiboboccibo@gmail.com>
2025-08-20 18:12:57 +0300

    Deleted some files

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,6 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
 
 import carrent from "./logo.svg";
 import jobit from "./logo.svg";
